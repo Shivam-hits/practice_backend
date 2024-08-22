1083a991885eb4fb98a89bf7cc098b1463ec7248
Shivam Awasthi <shivamhits1972@gmail.com>
2024-08-23 05:14:29 +0530

    edit product start

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,9 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
   });
 };
 
@@ -19,6 +16,14 @@ exports.postAddProduct = (req, res, next) => {
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+  });
+};
+
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
